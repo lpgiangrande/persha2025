f7fa7e48aab3d375fa0d19bf0b80ed08d9aa4a67
Laura G <62339671+lpgiangrande@users.noreply.github.com>
2024-10-13 04:04:59 +0200

    lenis library for smooth scroll, remove unused js code

--- assets/js/scrollHomepage.js
+++ assets/js/scrollHomepage.js
@@ -1,28 +1,16 @@
-let currentSection = 0; // Section actuellement visible
-const sections = document.querySelectorAll('.section');
+// Initialize Lenis
+const lenis = new Lenis();
 
-function scrollToSection(index) {
-    if (index >= 0 && index < sections.length) {
-        sections[index].scrollIntoView({ behavior: 'smooth' });
-        currentSection = index;
-    }
+// Listen for the scroll event and log the event data
+lenis.on('scroll', (e) => {
+  console.log(e);
+});
+
+// Use requestAnimationFrame to continuously update the scroll
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
 }
 
-window.addEventListener('wheel', (event) => {
-    if (event.deltaY > 0) {
-        // Défilement vers le bas
-        scrollToSection(currentSection + 1);
-    } else {
-        // Défilement vers le haut
-        scrollToSection(currentSection - 1);
-    }
-});
+requestAnimationFrame(raf);
 
-// Ajoute une écoute pour les flèches du clavier
-window.addEventListener('keydown', (event) => {
-    if (event.key === 'ArrowDown') {
-        scrollToSection(currentSection + 1);
-    } else if (event.key === 'ArrowUp') {
-        scrollToSection(currentSection - 1);
-    }
-});
