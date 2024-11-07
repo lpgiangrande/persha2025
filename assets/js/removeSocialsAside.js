cd86cb8107d825431479046f14a4ef3a35c4d5e6
Laura G <62339671+lpgiangrande@users.noreply.github.com>
2024-11-07 22:41:08 +0100

    reorganisation fichers css

---
+++ assets/js/removeSocialsAside.js
@@ -0,0 +1,17 @@
+// function toggleAside() {
+//     const aside = document.querySelector('aside.social-icons');
+//     if (window.innerWidth <= 768) {
+//         if (aside) {
+//             aside.remove(); 
+//         }
+//     } else {
+//         // Si vous souhaitez ajouter l'élément à nouveau lorsque la taille de l'écran est supérieure à 768px,
+//         // vous pouvez le réinsérer ici, mais cela nécessiterait une logique supplémentaire
+//         // pour conserver une référence à l'élément original.
+//     }
+// }
+
+// toggleAside();
+
+// // Ajouter un écouteur d'événements pour gérer les changements de taille de la fenêtre
+// window.addEventListener('resize', toggleAside);
